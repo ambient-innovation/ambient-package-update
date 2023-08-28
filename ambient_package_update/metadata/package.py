3a10f3f4bc9f96c3286bc63f0b1a561072c7def0
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2023-08-28 10:59:18 +0200

    v23.8.4

--- ambient_package_update/metadata/package.py
+++ ambient_package_update/metadata/package.py
@@ -1,7 +1,8 @@
 import dataclasses
-from typing import Dict, List
+import datetime
 
 from ambient_package_update.metadata.author import PackageAuthor
+from ambient_package_update.metadata.constants import LICENSE_MIT
 from ambient_package_update.metadata.readme import ReadmeContent
 from ambient_package_update.metadata.ruff_ignored_inspection import RuffIgnoredInspection
 
@@ -9,9 +10,12 @@ from ambient_package_update.metadata.ruff_ignored_inspection import RuffIgnoredI
 @dataclasses.dataclass
 class PackageMetadata:
     package_name: str
-    authors: List[PackageAuthor]
+    company: str
+    authors: list[PackageAuthor]
     development_status: str
     readme_content: ReadmeContent
-    dependencies: List[str]
-    optional_dependencies: Dict[str, List[str]] = None
-    ruff_ignore_list: List[RuffIgnoredInspection] = None
+    dependencies: list[str]
+    license: str = LICENSE_MIT
+    license_year: int = datetime.datetime.now(tz=datetime.UTC).year
+    optional_dependencies: dict[str, list[str]] = None
+    ruff_ignore_list: list[RuffIgnoredInspection] = None
