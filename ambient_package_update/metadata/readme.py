f4a5dadf0f56c6c7df42e917f438edfd8337b51e
Ronny V <ronny.vedrilla@ambient.digital>
2024-06-20 15:08:35 +0200

    Update ambient_package_update/metadata/readme.py

--- ambient_package_update/metadata/readme.py
+++ ambient_package_update/metadata/readme.py
@@ -5,7 +5,7 @@ from typing import Optional
 @dataclasses.dataclass
 class ReadmeContent:
     # Variables that are used in the default templates
-    tagline: str
+    tagline: str = None
     content: Optional[str] = None
     custom_installation: Optional[str] = None
     additional_installation: Optional[str] = None
